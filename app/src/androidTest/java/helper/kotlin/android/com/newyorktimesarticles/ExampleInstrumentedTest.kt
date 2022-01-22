501241690061352ed6cbcfe99c5151c5bb705782
samreen-ssp <samreen.kanwl@gmail.com>
2022-01-22 23:40:21 +0400

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/helper/kotlin/android/com/newyorktimesarticles/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/helper/kotlin/android/com/newyorktimesarticles/ExampleInstrumentedTest.kt
@@ -7,6 +7,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.URL
+import java.net.URLConnection
+import java.nio.charset.Charset
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +27,21 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("helper.kotlin.android.com.newyorktimesarticles", appContext.packageName)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testAvailability() {
+        val connection: URLConnection =
+            URL("http://www.mocky.io/v2/573c89f31100004a1daa8adb").openConnection()
+        val response: InputStream = connection.getInputStream()
+        val buffer = StringBuffer()
+        BufferedReader(InputStreamReader(response, Charset.defaultCharset())).use { reader ->
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                buffer.append(line)
+            }
+        }
+        assert(buffer.length > 0)
     }
 }
